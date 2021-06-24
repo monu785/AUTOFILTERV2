8460e44dc83e0285a7fe256a2b396ed160571cc5
monu785 <85724900+monu785@users.noreply.github.com>
2021-06-24 23:34:20 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -23,7 +23,7 @@ async def start(bot, update):
         if (file_id or file_type) == None:
             return
         
-        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>")
+        caption = file_caption if file_caption != https://t.me/newmoviesbn else ("<code>" + file_name + "</code>")
         
         if file_type == "document":
         
@@ -38,7 +38,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'our group join', url="https://t.me/newmoviesbn"
                                 )
                         ]
                     ]
